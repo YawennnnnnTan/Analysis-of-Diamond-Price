b196f04823b2ad4fc168fa4ceee235b97ad9d3bf
Yawen Tan <yawen.tan@mail.utoronto.ca>
2024-12-02 17:13:43 -0500

    update test cleaned data

--- scripts/04-test_analysis_data.R
+++ scripts/04-test_analysis_data.R
@@ -1,69 +1,86 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 26 September 2024 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Tests the structure and validity of the cleaned data
+# Author: Yawen Tan
+# Date: 30 November 2024
+# Contact: yawen.tan@utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: 
+# - The `tidyverse` package must be installed and loaded
+# - 03-clean_data.R must have been run
 
 #### Workspace setup ####
 library(tidyverse)
-library(testthat)
 
-data <- read_csv("data/02-analysis_data/analysis_data.csv")
+cleaned_data <- read_parquet(here("data/02-analysis_data/cleaned_diamondprice.parquet"))
+
+
+# Test if the data was successfully loaded
+if (exists("cleaned_data")) {
+  message("Test Passed: The dataset was successfully loaded.")
+} else {
+  stop("Test Failed: The dataset could not be loaded.")
+}
 
 
 #### Test data ####
-# Test that the dataset has 151 rows - there are 151 divisions in Australia
-test_that("dataset has 151 rows", {
-  expect_equal(nrow(analysis_data), 151)
-})
 
-# Test that the dataset has 3 columns
-test_that("dataset has 3 columns", {
-  expect_equal(ncol(analysis_data), 3)
-})
+# Check if the dataset has 2690 rows
+if (nrow(cleaned_data) == 2690) {
+  message("Test Passed: The dataset has 2690 rows.")
+} else {
+  stop("Test Failed: The dataset does not have 2690 rows.")
+}
 
-# Test that the 'division' column is character type
-test_that("'division' is character", {
-  expect_type(analysis_data$division, "character")
-})
+# Check if the dataset has 5 columns
+if (ncol(cleaned_data) == 5) {
+  message("Test Passed: The dataset has 5 columns.")
+} else {
+  stop("Test Failed: The dataset does not have 5 columns.")
+}
 
-# Test that the 'party' column is character type
-test_that("'party' is character", {
-  expect_type(analysis_data$party, "character")
-})
 
-# Test that the 'state' column is character type
-test_that("'state' is character", {
-  expect_type(analysis_data$state, "character")
-})
+# Check if the 'color' column contains only valid diamond color names
+valid_color <- c('D','E','F','G','H','I','J','K')
 
-# Test that there are no missing values in the dataset
-test_that("no missing values in dataset", {
-  expect_true(all(!is.na(analysis_data)))
-})
+if (all(cleaned_data$color %in% valid_color)) {
+  message("Test Passed: The 'color' column contains only valid diamond color names.")
+} else {
+  stop("Test Failed: The 'color' column contains invalid color names.")
+}
 
-# Test that 'division' contains unique values (no duplicates)
-test_that("'division' column contains unique values", {
-  expect_equal(length(unique(analysis_data$division)), 151)
-})
 
-# Test that 'state' contains only valid Australian state or territory names
-valid_states <- c("New South Wales", "Victoria", "Queensland", "South Australia", "Western Australia", 
-                  "Tasmania", "Northern Territory", "Australian Capital Territory")
-test_that("'state' contains valid Australian state names", {
-  expect_true(all(analysis_data$state %in% valid_states))
-})
+# Check if 'clarity' column contains only valid diamond clarity names
+valid_color <- c('IF','VVS1','VVS2','VS1','VS2','SI1','SI2')
 
-# Test that there are no empty strings in 'division', 'party', or 'state' columns
-test_that("no empty strings in 'division', 'party', or 'state' columns", {
-  expect_false(any(analysis_data$division == "" | analysis_data$party == "" | analysis_data$state == ""))
-})
+if (all(cleaned_data$clarity %in% valid_color)) {
+  message("Test Passed: The 'clarity' column contains only valid diamond clarity names.")
+} else {
+  stop("Test Failed: The 'clarity' column contains invalid clarity names.")
+}
 
-# Test that the 'party' column contains at least 2 unique values
-test_that("'party' column contains at least 2 unique values", {
-  expect_true(length(unique(analysis_data$party)) >= 2)
-})
+
+# Check if 'cut' column contains only valid diamond cut names
+valid_cut <- c('Ideal','Good','Very Good','Excellent')
+
+if (all(cleaned_data$cut %in% valid_cut)) {
+  message("Test Passed: The 'cut' column contains only valid diamond cut names.")
+} else {
+  stop("Test Failed: The 'color' column contains invalid cut names.")
+}
+
+
+# Check if the 'price' column contains non-negative value
+if (all(cleaned_data$price >=0)) {
+  message("Test Passed: The 'price' column contains non-negative value")
+} else {
+  stop("Test Failed: The 'price' column contains negative value")
+}
+
+
+
+# Check if there are any missing values in the dataset
+if (all(!is.na(cleaned_data))) {
+  message("Test Passed: The dataset contains no missing values.")
+} else {
+  stop("Test Failed: The dataset contains missing values.")
+}
